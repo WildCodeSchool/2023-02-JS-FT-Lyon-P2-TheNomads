e93d8c562f9bc3e0e1c6fe5976831f538c4bb37a
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-05-04 09:44:29 +0200

    Create recipes pages

--- frontend/src/pages/Recipes/Recipes.jsx
+++ frontend/src/pages/Recipes/Recipes.jsx
@@ -1,12 +1,43 @@
-import Header from "../../Header";
-import Footer from "../../Footer";
+import { useContext, useEffect, useState } from "react";
+import NewsContext from "../../contexts/NewsContext";
+import styles from "./Recipes.module.css";
 
 export default function Recipes() {
+  const { country } = useContext(NewsContext);
+
+  // const [codeFood, setCodeFood] = useState(null);
+  const [food, setFood] = useState(null);
+
+  const handleRecipe = (data) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${data}`)
+      .then((response) => response.json())
+      .then((res) => setFood(res.meals[0]));
+  };
+
+  const handleCodeFood = () => {
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?a=${country.food}`
+    )
+      .then((response) => response.json())
+      .then((data) => handleRecipe(data.meals[0].idMeal));
+    // .then((data) => console.log(data.meals))
+  };
+
+  useEffect(() => {
+    handleCodeFood();
+  }, []);
+
   return (
     <>
-      <Header />
       <div> RECIPES </div>
-      <Footer />
+      {country.food}
+      {food && (
+        <div className={styles.recipesContainer}>
+          <h1>Name: {food.strMeal}</h1>
+          <h2>Category: {food.strCategory}</h2>
+          <p>Instructions: {food.strInstructions}</p>
+        </div>
+      )}
     </>
   );
 }
